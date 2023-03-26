f5ddc9632760d2f071410ee6fe61f2af44e05f5c
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2023-03-26 06:35:33 +0300

    Add code for  the second lesson

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -1,17 +1,33 @@
 package ru.sumin.coroutinestart
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import ru.sumin.coroutinestart.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
-//выносим действия в отдельный поток
+//Handler - для обменов данными меду потоками
+//Looper - очередь сообщений
+//Handler - deprecated (при его создании не указывается явно в каком потоке будет обрабатываться сообщение =>
+//может быть краш => испольщуем Looper)
+//runOnUiThread использует под копотом Handler
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //главный поток
+    private val handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            println("HANDLE_MSG $msg")
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +36,13 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             loadData()
         }
+        handler.sendMessage(Message.obtain(handler, 0, 1))
     }
 
     private fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
         loadCity {
-            //как только выполнится, зайдем сюда
             binding.tvLocation.text = it
             loadTemperature(it) {
                 binding.tvTemperature.text = it.toString()
@@ -39,19 +55,27 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moskow")
+            runOnUiThread { callback.invoke("Moskow") }
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+            // Looper.prepare()
+            //главный поток
+            //Handler(Looper.getMainLooper())
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
             Thread.sleep(5000)
-            callback.invoke(17)
+            runOnUiThread {
+                callback.invoke(17)
+            }
         }
     }
 }
